5972d5cd52ee62a8500e2a60ddf0d06eeb76c5bc
Ruth Gomez <rdaniellagomez02@gmail.com>
2019-07-08 23:20:56 -0400

    Project files uploaded

---
+++ AnagramSecond/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AnagramSecond
+{
+    public class Program
+    {
+        public List<string> ReadFile(List<string> anagramsList)
+        {
+            var file = File.ReadAllLines(@"C:\Users\rdaniellagomez\anagrams.txt");
+            foreach (string line in file)
+            {
+                var words = line.Split(' ');
+                foreach (var word in words)
+                {
+                    anagramsList.Add(word);
+                }
+            }
+            return anagramsList;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ AnagramTest/Class1.cs
@@ -0,0 +1,19 @@
+﻿using AnagramSecond;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace AnagramTest
+{
+    [TestFixture]
+    public class AnagramTest
+    {
+        [Test]
+        public void Empty_File_Should_Return_Exception()
+        {
+            List<string> anagramFile = new List<string>();
+            Program anagram = new Program();
+            anagram.ReadFile(anagramFile);
+            Assert.IsNotEmpty(anagramFile);
+        }
+    }
+}
